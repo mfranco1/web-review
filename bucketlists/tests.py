d81eac16be1e8dbf883fe36a2690f2900984c0cd
mfranco1 <mcfranco16@gmail.com>
2018-08-13 22:10:17 +0800

    added retrieve, update, delete apis

--- bucketlists/tests.py
+++ bucketlists/tests.py
@@ -34,3 +34,40 @@ class BucketlistViewTestCase(TestCase):
     def test_view_can_create_bucketlist(self):
         """Test the api can create a bucketlist"""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_view_can_get_bucketlist(self):
+        """Test the api can retrieve a bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+            reverse('details',
+                    kwargs={'pk': bucketlist.id}),
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_view_can_update_bucketlist(self):
+        """Test the api can update a bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        change_bucketlist = {'name': 'new_test_name'}
+        response = self.client.put(
+            reverse('details',
+                    kwargs={'pk': bucketlist.id}),
+            change_bucketlist,
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_view_can_delete_bucketlist(self):
+        """Test the api can delete a bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details',
+                    kwargs={'pk': bucketlist.id}),
+            format="json",
+            follow=True
+        )
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
