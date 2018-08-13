3c09f89f51573fbde6c5c058b45aa19d6abbf343
mfranco1 <thekeyofm@gmail.com>
2018-08-13 21:26:25 +0800

    Release 1 - add create bucketlist API (#1)
    * adding bucketlists app
    
    * added bucketlist model
    
    * added unit test for create
    
    * added bucketlist serializer
    
    * add create bucketlist api

---
+++ bucketlists/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+class BucketlistCreateView(generics.ListCreateAPIView):
+    """Defines the create behavior of the bucketlist api"""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Create and save new bucketlist from post data"""
+        serializer.save()
