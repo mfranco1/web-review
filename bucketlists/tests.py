2df22e4f6176c3ac6f5fd89013fe7a42e888183e
mfranco1 <mcfranco16@gmail.com>
2018-08-13 20:29:25 +0800

    added unit test for create

--- bucketlists/tests.py
+++ bucketlists/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Bucketlist
 
-# Create your tests here.
+class BucketlistTestCase(TestCase):
+    """This class defines the test suite for the bucketlist model"""
+
+    def setUp(self):
+        self.bucketlist_name = "Sevatar"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name)
+
+    def test_model_can_create_bucketlist(self):
+        """Test bucketlist model can create a bucketlist"""
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
