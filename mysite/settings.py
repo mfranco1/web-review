3b9c5f1afa872863fd147a2d9c8a44e7593a8f68
mfranco1 <mcfranco16@gmail.com>
2018-08-22 15:23:05 +0800

    migrated to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('WEBREVIEW_DB_NAME', ''),
+        'USER': os.environ.get('WEBREVIEW_DB_USER', ''),
+        'PASSWORD': os.environ.get('WEBREVIEW_DB_PASSWORD', ''),
+        'HOST': os.environ.get('WEBREVIEW_DB_HOST', ''),
+        'PORT': os.environ.get('WEBREVIEW_DB_PORT', ''),
     }
 }
 
